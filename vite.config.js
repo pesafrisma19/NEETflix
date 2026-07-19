cd18025f0451722620cd393a0196f47c18a2898a
pesafrisma19 <pesafrismaagustina@gmail.com>
2026-07-19 22:34:42 +0700

    lgooooo

--- vite.config.js
+++ vite.config.js
@@ -18,12 +18,12 @@ export default defineConfig({
         display: 'standalone',
         icons: [
           {
-            src: '/logo.png',
+            src: '/logoN.png',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: '/logo.png',
+            src: '/logoN.png',
             sizes: '512x512',
             type: 'image/png'
           }
