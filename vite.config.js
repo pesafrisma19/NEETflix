75af92b7985dcdeab43fc6cf9d6d405cd9c51c3c
pesafrisma19 <pesafrismaagustina@gmail.com>
2026-07-19 21:50:52 +0700

    feat: turn application into PWA with standalone display

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,36 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
+import { VitePWA } from "vite-plugin-pwa"
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      injectRegister: 'auto',
+      manifest: {
+        name: 'NEETflix',
+        short_name: 'NEETflix',
+        description: 'Nonton Anime, Film, dan Baca Komik',
+        theme_color: '#191826',
+        background_color: '#191826',
+        display: 'standalone',
+        icons: [
+          {
+            src: '/logo.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/logo.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./"),
