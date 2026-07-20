8aaa9aa2e49b9cb62f794955061a59c31e0655e2
pesafrisma19 <pesafrismaagustina@gmail.com>
2026-07-20 11:52:09 +0700

    bhbhbhbh

--- vite.config.js
+++ vite.config.js
@@ -16,18 +16,66 @@ export default defineConfig({
         theme_color: '#191826',
         background_color: '#191826',
         display: 'standalone',
+        orientation: 'any',
+        id: '/',
         start_url: '/',
         scope: '/',
+        categories: ['entertainment', 'movies'],
+        dir: 'ltr',
+        shortcuts: [
+          {
+            name: 'Beranda Anime',
+            short_name: 'Beranda',
+            description: 'Buka halaman utama',
+            url: '/home',
+            icons: [{ src: '/logon-192.png', sizes: '192x192' }]
+          },
+          {
+            name: 'Cari Anime',
+            short_name: 'Cari Anime',
+            description: 'Cari judul anime',
+            url: '/search',
+            icons: [{ src: '/logon-192.png', sizes: '192x192' }]
+          },
+          {
+            name: 'Nonton Film',
+            short_name: 'Film',
+            description: 'Buka halaman Film',
+            url: '/film',
+            icons: [{ src: '/logon-192.png', sizes: '192x192' }]
+          },
+          {
+            name: 'Baca Komik',
+            short_name: 'Komik',
+            description: 'Buka halaman Komik',
+            url: '/comic',
+            icons: [{ src: '/logon-192.png', sizes: '192x192' }]
+          }
+        ],
         icons: [
           {
-            src: '/logon2.png',
+            src: '/logon-192.png',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: '/logon2.png',
+            src: '/logon-512.png',
             sizes: '512x512',
             type: 'image/png'
+          }
+        ],
+        screenshots: [
+          {
+            src: '/homepage2.webp',
+            sizes: '1920x1080',
+            type: 'image/webp',
+            form_factor: 'wide'
+          },
+          {
+            src: '/watchpage2.webp',
+            sizes: '1920x1080',
+            type: 'image/webp',
+            form_factor: 'wide'
           }
         ]
       }
